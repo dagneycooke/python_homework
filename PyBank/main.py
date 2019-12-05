c179829d945e2cd37093ff206f404c5b4f594ee6
Dagney Cooke <14912982+dagneycooke@users.noreply.github.com>
2019-12-05 13:53:58 -0800

    homework

---
+++ PyBank/main.py
@@ -0,0 +1,100 @@
+#pybank homework
+
+import os
+import csv
+
+# read the data file
+csvpath = os.path.join("Resources", "budget_data.csv")
+
+# create a function to calculate average from a list of numbers
+def average(list):
+    tally = 0
+    count = 0
+    for num in list:
+        tally = tally + int(num)
+        count = count + 1
+    return round(tally/count,2)
+
+with open(csvpath, 'r') as csvfile:
+
+    # read the csvfile
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader,None)
+
+    profit_loss = [] # create list to copy the data from the excel spreadsheet
+
+    # holds the value of the greatest profit increase
+    greatest_profit_increase = 0
+    # holds the value of the greatest profit decrease
+    greatest_profit_decrease = 0
+    # holds the value for the total number of months
+    months = []
+    # holds the vaue for the profit change month by month
+    change = []
+
+    # read data from all rows in csv reader
+    for row in csvreader: 
+        months.append(row[0]) # add months to list
+        profit_loss.append(int(row[1])) # add all profit/loss data to list
+
+    # calculate the change in profit/loss month over month
+    first_value = profit_loss[0]
+    second_value = 0
+    count = 0 # set value to 0 to loop through months
+    for value in profit_loss:
+        second_value = value
+        profit_change = second_value - first_value
+        change.append(profit_change)
+        first_value = second_value
+
+        # if the profit is greater than the already saved value, save over it, and save the month it's associate with
+        if profit_change > greatest_profit_increase:
+            greatest_profit_increase = profit_change
+            greatest_increase = months[count]
+        
+        # if the loss is greater than the already saved value, save over it, and save the month it's associate with
+        if profit_change < greatest_profit_decrease:
+            greatest_profit_decrease = profit_change
+            greatest_decrease = months[count]
+        
+        # increase month count
+        count = count + 1
+
+    # remove the first value from the list to compensate for the lack of change into the first month of the data set    
+    change.pop(0)
+
+# establish path to output text file
+output_path = os.path.join("output.txt")
+
+# open path to output text file
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the first row (column headers)
+    csvwriter.writerow(['Financial Analysis'])
+    csvwriter.writerow(['-----------------'])
+    csvwriter.writerow(['Total Months', len(months)]) # length of months list is number of months in file
+    csvwriter.writerow(['Total Profit', sum(profit_loss)]) # sum the total net profit
+    csvwriter.writerow(['Average Change', average(change)]) # average the list of change in profit for all months
+    csvwriter.writerow(['Greatest Increase in Profits', greatest_increase, greatest_profit_increase]) # write the greatest increase in profits
+    csvwriter.writerow(['Greatest Decrease in Profits', greatest_decrease, greatest_profit_decrease]) # write the greatest decrease in profits
+        
+    # print financial analysis to terminal
+    print("Financial Analysis")
+    print("-----------------------------")
+    print("Total Months: " + str(len(months)))
+    print("Total: $" + str(sum(profit_loss)))
+    print("Average Change: $" + str(average(change)))
+    print("Greatest Increase in Profits: " + greatest_increase + " $(" + str(greatest_profit_increase) + ")")
+    print("Greatest Decrease in Profits: " + greatest_decrease + " $(" + str(greatest_profit_decrease) + ")")
+
+# what the output should look like (from the readme file)
+#     Financial Analysis
+# ----------------------------
+# Total Months: 86
+# Total: $38382578
+# Average  Change: $-2315.12
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($-2196167)
---
+++ PyPoll/main.py
@@ -0,0 +1,107 @@
+#pypoll homework
+
+import os
+import csv
+
+# find file path to election_data.csv
+csvpath = os.path.join("Resources", "election_data.csv")
+
+# open csv file
+with open(csvpath, 'r') as csvfile:
+
+    # read in csv file
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader,None)
+
+    total_votes = 0 # set total votes to zero
+    num_candidates = 0 # set number of candidats to zero
+    candidates = [] # create list for candidates
+    
+
+    # read through csv file
+    for row in csvreader:
+        # increase total votes by one for each row in the file
+        total_votes = total_votes + 1
+        person = row[2]
+        # check to see if candidate has already been voted for, and if not, add them to the list of candidates and increase candidate count by one
+        if person not in candidates:
+            candidates.append(row[2])
+            num_candidates = num_candidates + 1
+
+    votes = [0]*num_candidates # create list for vote tracking
+# read through csv file again
+with open(csvpath, 'r') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader,None)
+
+    # now that the number of candidates has been established, count votes by candidate    
+    for row in csvreader:
+        for i in range(num_candidates):
+            if row[2] == candidates[i]:
+                votes[i] = votes[i] + 1
+    
+    percent_votes = [] # create list for percent of votes per candidate
+    highest_win = 0 # variable to track the highest percentage
+    position = 0
+    count = 0
+
+    # calculate percent votes for each candidate
+    for people in votes:
+        win_percent = round((people/total_votes)*100,3) # round percentage to two decimal places
+        percent_votes.append(win_percent) # add win percentage to list of percentages
+        if win_percent > highest_win: # keep track of the highest percentage
+            highest_win = win_percent
+            position = count # find the position in the list that matches the highest percentage
+        count = count + 1
+
+
+print("Election Results")
+print("-----------------------------")
+print("Total Votes: " + str(total_votes))
+print("-----------------------------")
+
+# loop through candidates and print their percentage and total votes
+for i in range(num_candidates):
+    print(str(candidates[i]) + ": " + str(percent_votes[i]) + "% (" + str(votes[i]) + ")")
+print("-----------------------------")
+print("Winner: " + str(candidates[position]))
+print("-----------------------------")
+
+# establish path to output text file
+output_path = os.path.join("output.txt")
+
+# open path to output text file
+with open(output_path, 'w', newline='') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')
+
+    # Write the first row (column headers)
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow(["-----------------------------"])
+    csvwriter.writerow(["Total Votes: " + str(total_votes)])
+    csvwriter.writerow(["-----------------------------"])
+
+# loop through candidates and print their percentage and total votes
+    i = 0
+    for i in range(num_candidates):
+        csvwriter.writerow([(str(candidates[i]) + ": " + str(percent_votes[i]) + "% (" + str(votes[i]) + ")")])
+
+    csvwriter.writerow(["-----------------------------"])
+    csvwriter.writerow(["Winner: " + str(candidates[position])])
+    csvwriter.writerow(["-----------------------------"])
+
+# what the output should look like (From readme file)
+
+# Election Results
+# -------------------------
+# Total Votes: 3521001
+# -------------------------
+# Khan: 63.000% (2218231)
+# Correy: 20.000% (704200)
+# Li: 14.000% (492940)
+# O'Tooley: 3.000% (105630)
+# -------------------------
+# Winner: Khan
+# -------------------------
